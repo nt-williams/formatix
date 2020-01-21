0e629411bc2b836ec3930cbea0e7fe375a5b5d37
Nick Williams <ntwilliams.personal@gmail.com>
2020-01-21 09:58:36 -0500

    initial files added

---
+++ R/formatix.R
@@ -0,0 +1,40 @@
+
+#' Combine bounds for presentation.
+#'
+#' @param df a dataframe or tibble
+#' @param label name of the new column
+#'
+#' @return
+#' @export
+combine_ci <- function(df, label) {
+  out <- unite(df, !! label, contains("low"), contains("high"), sep = ", ")
+  out <- mutate(out, !! label := paste0("(", .data[[label]], ")"))
+  return(out)
+}
+
+#' Format digits to be a specific length.
+#'
+#' @param x a numeric object
+#' @param n the number of digits to follow the decimal
+#'
+#' @return a character representation of the original input with the formatted digits
+#' @export
+#'
+#' @examples
+#' format_digits(1.34523435353, 2)
+format_digits <- function(x, n) {
+  format(round(as.double(x), digits = n), nsmall = n, trim = TRUE)
+}
+
+#' Wrap text in paranthesis.
+#'
+#' @param x a character or numeric value
+#'
+#' @return a character representation of the original input surrounded in parenthesis
+#' @export
+#'
+#' @examples
+#' wrap_parenthesis(55)
+wrap_parenthesis <- function(x) {
+  paste0("(", x, ")")
+}
